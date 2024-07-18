fb5855ce3202a0e88f5024ca415794d3949f7830
Raj <amritrjakacabu015@gmail.com>
2024-07-17 21:35:19 -0400

    Code refactoring.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <cstring>
-#include "pixar.cpp"
+#include "pixar.h"
 
 int main() {
     size_t width = 800, height = 800;
@@ -8,23 +8,22 @@ int main() {
     const char *lineFileName = "lines.ppm";
 
     Px::Surface *surface = Px::Surface::create(width, height);
-    Px::fill(surface, 0x00FFBCBC);
+    surface->apply_color(0x00FFBCBC);
 
     Px::Rectangle rect = { .width = 600, .height = 600, .x = 100, .y = 100 };
-    Px::fill_rectangle(surface, &rect, 0x000000FF);
+    surface->draw_rectangle(&rect, 0x000000FF);
 
     rect.width = rect.height = 400;
     rect.x = rect.y = 200;
-
-    Px::fill_rectangle(surface, &rect, 0x0000FFFF);
+    surface->draw_rectangle(&rect, 0x0000FFFF);
 
     rect.width = rect.height = 200;
     rect.x = rect.y = 300;
+    surface->draw_rectangle(&rect, 0x00FF00FF);
 
-    Px::fill_rectangle(surface, &rect, 0x00FF00FF);
-    Px::Errno err = Px::save_as_ppm(surface, rectanglesFileName);
+    Px::Errno err = surface->save_as_ppm(rectanglesFileName);
 
-    Px::fill(surface, 0x00000000);
+    surface->apply_color(0x00000000);
     Px::Circle circle = { .radius = 10, .x = 0, .y = 0 };
 
     for (int x = 0; x < width; x += 100) {
@@ -34,36 +33,36 @@ int main() {
         for (int y = 0; y < height; y += 100) {
             circle.y = y + 50;
 
-            Px::fill_circle(surface, &circle, 0x0000FF00);
+            surface->draw_circle(&circle, 0x0000FF00);
         }
     }
 
-    err = Px::save_as_ppm(surface, circlesFileName);
+    err = surface->save_as_ppm(circlesFileName);
 
-    Px::fill(surface, 0x00000000);
+    surface->apply_color(0x00000000);
     Px::Line line = { .x1 = 0, .y1 = 200, .x2 = 799, .y2 = 200 };
-    Px::draw_line(surface, &line, 0x00FF0000);
+    surface->draw_line(&line, 0x00FF0000);
 
     line.y1 = line.y2 = 600;
-    Px::draw_line(surface, &line, 0x0000FF00);
+    surface->draw_line(&line, 0x0000FF00);
 
     line.y1 = 0;
     line.y2 = 799;
     line.x1 = line.x2 = 200;
-    Px::draw_line(surface, &line, 0x00A000FF);
+    surface->draw_line(&line, 0x00A000FF);
 
     line.x1 = line.x2 = 600;
-    Px::draw_line(surface, &line, 0x00F0FF0F);
+    surface->draw_line(&line, 0x00F0FF0F);
 
     line.x1 = line.y1 = 0;
     line.x2 = line.y2 = 799;
-    Px::draw_line(surface, &line, 0x00FFFFFF);
+    surface->draw_line(&line, 0x00FFFFFF);
 
     line.x1 = line.y2 = 799;
     line.x2 = line.y1 = 0;
-    Px::draw_line(surface, &line, 0x00FFFFFF);
+    surface->draw_line(&line, 0x00FFFFFF);
 
-    err = Px::save_as_ppm(surface, lineFileName);
+    err = surface->save_as_ppm(lineFileName);
 
     if (err) {
         printf("Could not save file %s, %s\n", rectanglesFileName, strerror(err));
